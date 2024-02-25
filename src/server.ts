8dc80131f0696a46f6ccd5558fc9032684c0b852
Tamicktom <rickpokemon38@gmail.com>
2024-02-25 14:03:51 -0300

    saving

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify";
 
 const app = fastify({ logger: true });
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+app.listen({ port: 3000 }).then((address) => {
+  console.log(`Server listening on ${address}`);
+});
