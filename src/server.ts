21b9a2497bdff43a25f37989e0152e7be66977e9
Tamicktom <rickpokemon38@gmail.com>
2024-02-25 14:32:19 -0300

    feat - add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
 
+//* Local imports
+import { knex } from "./database";
+
 const app = fastify({ logger: true });
 
 app.get("/", async (request, reply) => {
-  return { hello: "world" };
+  const tables = await knex("sqlite_schema").select("*");
+
+  return tables;
 });
 
 app.listen({ port: 3000 }).then((address) => {
