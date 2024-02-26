0a3038e0246a841224ecdddde1156875c95212f8
Tamicktom <rickpokemon38@gmail.com>
2024-02-25 21:21:16 -0300

    add two rutes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -12,7 +12,29 @@ const createTransactionSchema = z.object({
   type: z.enum(["credit", "debit"]),
 });
 
+const getTransactionSchema = z.object({
+  id: z.string().uuid(),
+});
+
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select("*");
+
+    return { transactions };
+  });
+
+  app.get("/:id", async (request, reply) => {
+    const { id } = getTransactionSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    if (!transaction) {
+      return reply.status(404).send({ error: "Transaction not found" });
+    }
+
+    return { transaction };
+  });
+
   app.post("/", async (request, reply) => {
     const body = createTransactionSchema.parse(request.body);
 
