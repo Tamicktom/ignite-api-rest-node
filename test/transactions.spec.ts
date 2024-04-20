9f9dfedfb4f56350935e1c12b6d91ec5a0f1b2d3
Tamicktom <rickpokemon38@gmail.com>
2024-04-20 16:34:09 -0300

    saving

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,23 +1,27 @@
-import { test, beforeAll, afterAll } from "vitest";
+import { test, beforeAll, afterAll, describe } from "vitest";
 import supertest from "supertest";
 
 import { app } from "../src/app";
 
-beforeAll(async () => {
-  await app.ready();
-});
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-afterAll(async () => {
-  await app.close();
-});
+  afterAll(async () => {
+    await app.close();
+  });
 
-test("User can create a new transaction", async () => {
+  test("User can create a new transaction", async () => {
     await supertest(app.server)
       .post("/transactions")
       .send({
-      title: "New transaction",
-      amount: 5000,
-      type: "credit"
+        title: "New transaction",
+        amount: 5000,
+        type: "credit"
       })
       .expect(201)
+  });
 });
+
+
